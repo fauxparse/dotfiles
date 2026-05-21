7d9e51d353c42ab9e98e3c3524d523876e810e00
Matt Powell <matt.powell@usabilityhub.com>
2026-05-22 11:19:43 +1200

    add omzsh config

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+ln -sf "$DOTFILES_DIR/.p10k.zsh" "$HOME/.p10k.zsh"
+
+
